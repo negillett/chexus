95ed1e02a05ef3010300cbe8b8314fdd12be0235
Nathan Gillett <ngillett@redhat.com>
2020-01-26 16:55:45 -0500

    Fix MockedClient

--- tests/client/mocked_client.py
+++ tests/client/mocked_client.py
@@ -4,12 +4,19 @@ from chexus import Client
 
 
 class MockedClient(Client):
-    def __init__(self):
-        super(Client).__init__()
-        self._session = self.mock_session()
+    def __init__(
+        self,
+        access_id=None,
+        access_key=None,
+        session_token=None,
+        default_region=None
+    ):
+        with mock.patch("boto3.Session") as mocked_sessions:
+            mocked_sessions.return_value = mocked_sessions
+            Client.__init__(self, access_id, access_key, session_token, default_region)
 
     @staticmethod
-    def mock_session():
+    def mocked_session():
         fake_session = mock.MagicMock()
         fake_session.resource.Bucket.return_value = mock.MagicMock()
         fake_session.resource.Table.return_value = mock.MagicMock()
